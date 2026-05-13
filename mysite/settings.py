9a7a921585c88eac1187fac3b1b3eca18d9e44ff
benadrylmwangi <benadrylmwangi@gmail.com>
2026-05-13 15:23:08 +0300

    Add SITE_ID for allauth

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,8 @@ SECURE_SSL_REDIRECT = env_bool("SECURE_SSL_REDIRECT", default=False)
 SECURE_HSTS_SECONDS = int(env("SECURE_HSTS_SECONDS", 0))
 
 ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", default=[
-    "ledgerpro.com",
-    "www.ledgerpro.com",
+    "mysite-production-735a.up.railway.app",
+    "mysite-production-735a.up.railway.app",
     ".railway.app",
     "127.0.0.1",
     "localhost",
@@ -91,8 +91,8 @@ if APP_HOST and APP_HOST not in ALLOWED_HOSTS:
     ALLOWED_HOSTS.append(APP_HOST)
 
 CSRF_TRUSTED_ORIGINS = env_list("CSRF_TRUSTED_ORIGINS", default=[
-    "https://ledgerpro.com",
-    "https://www.ledgerpro.com",
+    "https://mysite-production-735a.up.railway.app",
+    "https://mysite-production-735a.up.railway.app",
     "https://*.railway.app",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
